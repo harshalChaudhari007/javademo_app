73dbb011c73fee38d7c71a4774f765cf24eb3db0
hchaudhari007 <hchaudhari007@gmail.com>
2023-01-13 10:02:39 +0530

    inital commit

---
+++ src/main/java/com/example/javademo_app/JavademoAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.javademo_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavademoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavademoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javademo_app/JavademoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javademo_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavademoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
